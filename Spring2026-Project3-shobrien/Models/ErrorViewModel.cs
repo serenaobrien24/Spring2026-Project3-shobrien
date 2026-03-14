f2291419fd00cd8095ba5ae157af2eecd19ea251
serenaobrien24 <shobrien@crimson.ua.edu>
2026-03-14 09:06:37 -0500

    Setting up project

---
+++ Spring2026-Project3-shobrien/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Spring2026_Project3_shobrien.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
