24646f8c067e3dcaa9234c3ef8a0891148f31093
serenaobrien24 <shobrien@crimson.ua.edu>
2026-03-14 10:13:53 -0500

    Attempting to set up local db stuff

--- Spring2026-Project3-shobrien/Data/ApplicationDbContext.cs
+++ Spring2026-Project3-shobrien/Data/ApplicationDbContext.cs
@@ -1,9 +1,27 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Spring2026_Project3_shobrien.Models;
 
 namespace Spring2026_Project3_shobrien.Data
 {
-    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+    public class ApplicationDbContext : IdentityDbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Actor> Actors { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<MovieActor> MovieActors { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<MovieActor>()
+                .HasKey(ma => new { ma.MovieID, ma.ActorID });
+        }
     }
+
 }
