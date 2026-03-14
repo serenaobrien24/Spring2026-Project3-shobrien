7c256e136809d9e190eda6ce4f898ae632da386d
serenaobrien24 <shobrien@crimson.ua.edu>
2026-03-14 10:58:24 -0500

    Adding basic Movies functionality

---
+++ Spring2026-Project3-shobrien/Controllers/MoviesController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Spring2026_Project3_shobrien.Data;
+using Spring2026_Project3_shobrien.Models;
+
+namespace Spring2026_Project3_shobrien.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public MoviesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var movies = await _context.Movies.ToListAsync();
+            return View(movies);
+        }
+    }
+}
