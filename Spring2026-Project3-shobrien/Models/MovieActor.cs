675cdade3368777d1e1174bc90cfd6171a5d0511
serenaobrien24 <shobrien@crimson.ua.edu>
2026-03-14 10:03:26 -0500

    Building out MovieActor

--- Spring2026-Project3-shobrien/Models/MovieActor.cs
+++ Spring2026-Project3-shobrien/Models/MovieActor.cs
@@ -2,6 +2,10 @@
 {
     public class MovieActor
     {
-       
+        public int MovieID { get; set; }
+        public int ActorID { get; set; }
+
+        public Actor Actor { get; set; }
+        public Movie Movie { get; set; }
     }
 }
