7c1eafcadb0b0bb7589345c07fecd06853a89c2d
serenaobrien24 <shobrien@crimson.ua.edu>
2026-03-26 18:59:00 -0500

    Killing off some remnants of the MVC template

--- Spring2026-Project3-shobrien/Controllers/HomeController.cs
+++ Spring2026-Project3-shobrien/Controllers/HomeController.cs
@@ -11,11 +11,6 @@ namespace Spring2026_Project3_shobrien.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
